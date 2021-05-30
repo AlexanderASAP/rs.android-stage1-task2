e8dbeaebabd1ee1830e627a430eb60a751e7afbf
AlexanderASAP <aleksavical@gmail.com>
2021-05-30 19:28:18 +0300

    To test

---
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -0,0 +1,32 @@
+package subtask3
+
+class StringParser {
+
+    // TODO: Complete the following function
+    fun getResult(inputString: String): Array<String> {
+        val result = mutableListOf<String>()
+        val lastIndex = inputString.length - 1
+
+        inputString.slice(0 until lastIndex).forEachIndexed loop@{ startIndex, startBracket ->
+
+            if (startBracket in startBrackets) {
+                val endBracket = startBrackets[startBracket]
+                var count = 1
+
+                inputString.slice(startIndex + 1..lastIndex).forEachIndexed { endIndex, bracket ->
+                    when (bracket) {
+                        startBracket -> count += 1
+                        endBracket -> count -= 1
+                    }
+                    if (count == 0) {
+                        result.add(inputString.substring(startIndex + 1, startIndex + 1 + endIndex))
+                        return@loop
+                    }
+                }
+            }
+        }
+
+        return result.toTypedArray()
+
+    }
+}
